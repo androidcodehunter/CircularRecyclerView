c34d3f3d64f441847a2fd195b086393dd66d2a4c
Sharif <sharif.iit.du@gmail.com>
2016-09-08 18:29:48 +0600

    place initial child

--- app/src/main/java/com/example/android/recyclerplayground/layout/MyListManager.java
+++ app/src/main/java/com/example/android/recyclerplayground/layout/MyListManager.java
@@ -71,36 +71,49 @@ public class MyListManager extends RecyclerView.LayoutManager{
 
         windowResize();
 
-        int childLeft = 0;
-        int childTop = 0;
+        int startLeftPadding = getPaddingLeft();
+        int startTopPadding = getPaddingTop();
 
         if (getChildCount() == 0) { //First or empty layout
             //Reset the visible and scroll positions
             mFirstVisiblePosition = 0;
-            childLeft = getPaddingLeft();
-            childTop = getPaddingTop();
+            startLeftPadding = getPaddingLeft();
+            startTopPadding = getPaddingTop();
         }
 
         //Clear all views
         detachAndScrapAttachedViews(recycler);
 
         //Fill the grid for the initial layout of views
-        fillGrid(DIRECTION_NONE, childLeft, childTop, recycler, state, null);
+        fillGrid(DIRECTION_NONE, startLeftPadding, startTopPadding, recycler, state, null);
 
     }
 
-    private void fillGrid(int direction, int childLeft, int childTop, RecyclerView.Recycler recycler, RecyclerView.State state, Object o) {
+    private void fillGrid(int direction, int recyclerTopLeftPadding, int recyclerTopPadding, RecyclerView.Recycler recycler, RecyclerView.State state, Object o) {
 
         Log.d(TAG, "only child count " + getChildCount() + " mVisibleRowCount " + mVisibleRowCount);
 
-        int startLeftOffset = 0;
-        int startTopOffset = 0;
+        if (mFirstVisiblePosition < 0) mFirstVisiblePosition = 0;
+        if (mFirstVisiblePosition >= getItemCount()) mFirstVisiblePosition = (getItemCount() - 1);
 
-        for (int i = 0; i<mVisibleRowCount; i++){
+
+        int startLeftOffset = recyclerTopLeftPadding;
+        int startTopOffset = recyclerTopPadding;
+
+        for (int i = 0; i<mVisibleRowCount; i++) {
             View view = recycler.getViewForPosition(i);
             addView(view);
 
-           // layoutDecorated(view,0,0,500,200);
+                  /*
+                 * It is prudent to measure/layout each new view we
+                 * receive from the Recycler. We don't have to do
+                 * this for views we are just re-arranging.
+                 */
+            measureChildWithMargins(view, 0, 0);
+
+            layoutDecorated(view,startLeftOffset, startTopOffset,startLeftOffset + mDecoratedChildWidth,startTopOffset + mDecoratedChildHeight);
+
+            startTopOffset += mDecoratedChildHeight;
         }
 
     }
@@ -124,6 +137,69 @@ public class MyListManager extends RecyclerView.LayoutManager{
     @Override
     public boolean canScrollVertically() {
         return true;
+    }
+
+    @Override
+    public int scrollVerticallyBy(int dy, RecyclerView.Recycler recycler, RecyclerView.State state) {
+
+        if (getChildCount() == 0){
+            return 0;
+        }
+
+        //Take top measurements from the top child
+        final View topView = getChildAt(0);
+        //Take bottom measurements from the bottom child.
+        final View bottomView = getChildAt(getChildCount()-1);
+
+
+        //Optimize the case where the entire data set is too small to scroll
+        int viewSpan = getDecoratedBottom(bottomView) - getDecoratedTop(topView);
+        if (viewSpan < getVerticalSpace()) {
+            //We cannot scroll in either direction
+            return 0;
+        }
+
+
+        int delta = 0;
+        int maxRowCount = mVisibleRowCount;
+        boolean topBoundReached = mFirstVisiblePosition == 0;
+        boolean bottomBoundReached = getLastVisibleRow() >= maxRowCount;
+Log.d(TAG, "bottom reached: " + bottomBoundReached);
+        //Check for up scroll
+        if (dy > 0){
+            int bottomOffset;
+            if (bottomBoundReached){
+
+                    //We are truly at the bottom, determine how far
+                    bottomOffset = getVerticalSpace() - getDecoratedBottom(bottomView)
+                            + getPaddingBottom();
+                delta = Math.max(-dy, bottomOffset);
+            } else {
+                //No limits while the last row isn't visible
+                delta = -dy;
+            }
+
+        }
+
+        offsetChildrenVertical(delta);
+
+
+        if (dy > 0) {
+            Log.d(TAG, "dy "+ dy);
+            if (getDecoratedBottom(topView) < 0 && !bottomBoundReached) {
+                Log.d(TAG, "if dy "+ dy);
+                fillGrid(DIRECTION_DOWN, 0, 0, recycler, state, null);
+            } else if (!bottomBoundReached) {
+                Log.d(TAG, "else dy "+ dy);
+                fillGrid(DIRECTION_NONE, 0, 0, recycler, state, null);
+            }
+        }
+
+        return -delta;
+    }
+
+    private int getLastVisibleRow() {
+        return mFirstVisiblePosition + mVisibleRowCount;
     }
 
     @Override
